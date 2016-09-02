25d78a082fe82870a480d5459cea7272cddd6d41
Nicholas Wang <nicowang9@gmail.com>
2016-09-02 15:05:56 -0400

    song serialize data for user/show and song/index

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,10 @@
 class SongsController < ApplicationController
   def index
+    user = User.find(current_user.id)
+    unfinished_songs = Song.where(finished: false)
+    finished_songs = Song.where(finished: true)
+    render json: user.as_json(include: [:genres, :talents ])
+    render json: unfinished_songs.as_json(include: [ :genres,:likes, :talents])
+    render json: finished_songs.as_json(include: [:likes])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,25 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    render json: user.as_json(include: [:talents, :comments], only: :username)
+    songs_collaborated = Song.where()
+    user_songs = Song.where(owner_id: current_user.id)
+    find_liked_songs
+    liked_songs = @songs
+    private_user_unfinished_songs = user_songs.where(finished: false)
+    private_user_finished_songs = user_songs.where(finished: true)
+    render json: user.as_json(include: [:talents, :comments, :followers, :following]
+    render json: private_user_finished_songs.as_json
+    render json: private_user_finished_songs.as_json
+    render json: liked_songs.as_json
+  end
+
+  private
+  def find_liked_songs
+    likes = Like.where(user_id: current_user.id)
+    @songs = []
+    likes.each do |like|
+      @songs << Song.find(like.song_id)
+      return @songs
+    end
   end
 end
