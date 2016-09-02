3b439664657287c79e27e93fe9fc656e7a361e5f
Nicholas Wang <nicowang9@gmail.com>
2016-09-02 16:30:00 -0400

    serialize data for songs#index

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,19 @@
 class SongsController < ApplicationController
-  def index
+  def finished_songs
+    finished_songs = Song.where(finished: true)
+    render json: finished_songs.as_json(include: [:likes, :genres, :desired_talents])
+  end
+
+  def unfinished_songs
     user = User.find(current_user.id)
     unfinished_songs = Song.where(finished: false)
-    finished_songs = Song.where(finished: true)
-    render json: user.as_json(include: [:genres, :talents ])
-    render json: unfinished_songs.as_json(include: [ :genres,:likes, :talents])
-    render json: finished_songs.as_json(include: [:likes])
+    @user_talents = user.talents
+    @logged_in = true if user_signed_in?
+    @user_genres = user.genres
+    respond_to do |format|
+      format.html {songsindex.html.erb}
+      format.xml {render json: unfinished_songs.as_json(include: [:desired_talents, :genres, :likes])}
+      end
   end
 
   def show
@@ -15,9 +23,9 @@ class SongsController < ApplicationController
     if song.owner_id = current_user.id
       @private_user_auth = true
     end
-    # @likes = song.likes.count
     @followers_number = user.followers.count
     render json: song.as_json(include:
-      [{master_tracks: { include: [:feature_tracks, :comments]}}, :user, :genres, :desired_talents, :feature_tracks])
+      [{master_tracks: { include: [:feature_tracks, :comments, :likes]}}, :user, :genres, :desired_talents, :feature_tracks])
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
-  root to: 'songs#index'
+  root to: 'songs#unfinished_songs'
   resources :users
   resources :songs
 end
