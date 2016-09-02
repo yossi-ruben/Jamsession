53a26b8973c65ac0adc99a837949d8b4a6d5bca7
Tony Narisi <tonynarisi@gmail.com>
2016-09-02 08:02:53 -0400

    Pass tests for master track

--- app/models/master_track.rb
+++ app/models/master_track.rb
@@ -1,14 +1,14 @@
 class MasterTrack < ApplicationRecord
   # Connects master track to song and owner of song
   belongs_to :song
-  has_one :song_owner, through: :song, class_name: "User"
+  has_one :song_owner, through: :song, source: :user
 
   # Connects master track to feature tracks
   has_many :master_features
   has_many :feature_tracks, through: :master_features
 
   # Connects master track to users who submitted feature tracks and the talents involved
-  has_many :contributors, through: :feature_tracks, class_name: "User"
+  has_many :contributors, through: :feature_tracks, source: :user
   has_many :talents, through: :feature_tracks
 
   # Connects master track to comments
@@ -16,5 +16,4 @@ class MasterTrack < ApplicationRecord
 
   # Connects master track to people who liked it
   has_many :likes
-  has_many :fans, through: :likes, class_name: "User"
 end
