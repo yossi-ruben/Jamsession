a4b8dd93c00929dc4bbf0b3c518eb7d960d3fc08
Tony Narisi <tonynarisi@gmail.com>
2016-09-04 12:22:43 -0400

    Implement ability to upload features

--- app/assets/javascripts/components/song-page/feature-submission.es6.jsx
+++ app/assets/javascripts/components/song-page/feature-submission.es6.jsx
@@ -2,10 +2,13 @@ class FeatureSubmission extends React.Component {
   constructor() {
     super();
     this.state = {
-      showSubmissionForm: false
+      showSubmissionForm: false,
+      fileUrl: "",
+      featureFile: null
     }
     this.toggleFormView = this.toggleFormView.bind(this);
     this.submitFeature = this.submitFeature.bind(this);
+    this.changeFile = this.changeFile.bind(this);
   }
 
   toggleFormView() {
@@ -14,28 +17,46 @@ class FeatureSubmission extends React.Component {
     })
   }
 
+  changeFile(e) {
+    var reader = new FileReader();
+    var file = e.target.files[0];
+    var that = this;
+
+    reader.onloadend = function() {
+      that.setState({ imageUrl: reader.result, imageFile: file });
+    }
+
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      this.setState({ imageUrl: "", imageFile: null});
+    }
+  }
+
   submitFeature(e) {
     e.preventDefault();
-    let descriptionField = this.refs.trackDescription;
-    let talentField = this.refs.trackTalent;
-    let fileField = this.refs.trackFile;
-    let data = {
-      description: descriptionField.value,
-      talent_id: talentField.value,
-      file: fileField.value,
-      user_id: this.refs.trackUser.value,
-      song_id: this.props.song.id
-    }
+
+    let userID = this.refs.trackUser.value
+    let description = this.refs.trackDescription.value
+    let talentID = this.refs.trackTalent.value
+    let file = this.state.imageFile
+    let songID = this.refs.trackSong.value
+
+    var formData = new FormData();
+    formData.append("feature_track[user_id]", userID);
+    formData.append("feature_track[description]", description);
+    formData.append("feature_track[talent_id]", talentID);
+    formData.append("feature_track[file]", file);
+    formData.append("feature_track[song_id]", songID);
+
     fetch(`/feature_tracks`, {
       method: "post",
       dataType: "JSON",
       headers: {
           "X-CSRF-Token": this.props.csrf,
-          "Accept": "application/json",
-          "Content-Type": "application/json"
         },
       credentials: "include",
-      body: JSON.stringify(data)
+      body: formData
     })
   }
 
@@ -53,7 +74,8 @@ class FeatureSubmission extends React.Component {
             <div className="form-holder">
               <h3>Upload a Feature Track</h3>
               <form encType="multipart/form-data" onSubmit={this.submitFeature}>
-                <input type="hidden" name="feature_track[user_id]" ref="trackUser" value={this.props.currentUser.id} />
+                <input type="hidden" name="feature_track[user_id]" ref="trackUser" value={this.props.song.id} />
+                <input type="hidden" name="feature_track[song_id]" ref="trackSong" value={this.props.song.id} />
                 <label htmlFor="feature_track[description]" className="form-label">Track Description:</label>
                 <textarea rows="5" cols="30" name="feature_track[description]" ref="trackDescription"></textarea>
                 <label htmlFor="feature_track[talent_id]" className="form-label">What talent does this track correspond with?</label>
@@ -62,7 +84,7 @@ class FeatureSubmission extends React.Component {
                     return <option value={talent.id} key={i}>{talent.title}</option>
                   })}
                 </select>
-                <input type="file" name="feature_track[file]" className="form-input" ref="trackFile"/>
+                <input type="file" name="feature_track[file]" className="form-input" ref="trackFile" onChange={this.changeFile}/>
                 <input type="submit" value="Submit Track" className="form-input"/>
               </form>
             </div>
--- app/controllers/feature_tracks_controller.rb
+++ app/controllers/feature_tracks_controller.rb
@@ -2,14 +2,14 @@ class FeatureTracksController < ApplicationController
   def create
     s3 = AWS::S3.new(:access_key_id => ENV['ACCESS_KEY_ID'], :secret_access_key => ENV['SECRET_ACCESS_KEY'])
 
-    obj = s3.buckets[ENV['S3_BUCKET']].objects["dummy"]
+    obj = s3.buckets[ENV['S3_BUCKET']].objects[params[:feature_track][:file].original_filename]
 
     obj.write(
-      file: params[:file],
+      file: params[:feature_track][:file],
       acl: :public_read
     )
 
-    song = Song.find_by(id: params[:song_id])
+    song = Song.find_by(id: params[:feature_track][:song_id])
 
     feature_track = song.feature_tracks.new(feature_track_params)
 
@@ -21,6 +21,6 @@ class FeatureTracksController < ApplicationController
 
   private
   def feature_track_params
-    params.require(:feature_track).permit(:file, :talent_id, :user_id, :file_name, :file_path, :description)
+    params.require(:feature_track).permit(:talent_id, :user_id, :description, :song_id)
   end
 end
