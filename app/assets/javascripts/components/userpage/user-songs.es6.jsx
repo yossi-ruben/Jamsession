18f82dd3d25b8fda0797b11a1761adafc7ec0fe7
Nicholas Wang <nicowang9@gmail.com>
2016-09-07 12:40:18 -0400

    background colors for userdetails and page

--- app/assets/javascripts/components/userpage/user-songs.es6.jsx
+++ app/assets/javascripts/components/userpage/user-songs.es6.jsx
@@ -26,14 +26,14 @@ class UserSongs extends React.Component {
         <div className="carousel-inner" role="listbox">
           <div >
             <a href={'/songs/' + info.id}><img src="https://image.freepik.com/free-icon/music-note_318-102209.png" alt="Chania" /></a>
-            <div className="carousel-caption">
+            <div>
               <a href={'/songs/' + info.id}>Song Title: {info.title}</a>
               <h5>Date: {useMonth + " " + day.toString() + " " + year.toString()}</h5>
-              <p>Description: {info.background}</p>
+              <p>Description of Song {info.background}</p>
               <button onClick={this.sendSongToParent}>Play</button>
             </div>
           </div>
-        </div>   
+        </div>
 
       </div>
     }
