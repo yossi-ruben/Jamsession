ae5e79a9fcfa4bfe017b6d038b7650a7101dcfd2
Nicholas Wang <nicowang9@gmail.com>
2016-09-07 12:13:24 -0400

    stylings the worst

--- app/assets/javascripts/components/userpage/user-finished-songs.es6.jsx
+++ app/assets/javascripts/components/userpage/user-finished-songs.es6.jsx
@@ -2,12 +2,15 @@ class UserFinishedSongs extends React.Component {
   render() {
     return (
       <div className="song">
-          <h3>Finished</h3>
+          <h3 className="finhead">Finished</h3>
+          <div id="myCarousel" className="carousel slide" data-ride="carousel">
             {this.props.finished.map((song, i) => {
               return(
                 <UserSongs info={song} playSong={this.props.playSong} key={i} />
                 )
             })}
+            </div>
+
       </div>
     )
   }
