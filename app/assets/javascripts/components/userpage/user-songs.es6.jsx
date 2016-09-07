98c7858199817127476e1fc515c1ad3e125da46c
Nicholas Wang <nicowang9@gmail.com>
2016-09-07 14:30:52 -0400

    fixed form spacing, add stlying for start_project

--- app/assets/javascripts/components/userpage/user-songs.es6.jsx
+++ app/assets/javascripts/components/userpage/user-songs.es6.jsx
@@ -26,7 +26,7 @@ class UserSongs extends React.Component {
         <div className="carousel-inner" role="listbox">
           <div >
             <a href={'/songs/' + info.id}><img src="https://image.freepik.com/free-icon/music-note_318-102209.png" alt="Chania" /></a>
-            <div>
+            <div className="">
               <a href={'/songs/' + info.id}>Song Title: {info.title}</a>
               <h5>Date: {useMonth + " " + day.toString() + " " + year.toString()}</h5>
               <p>Description of Song {info.background}</p>
