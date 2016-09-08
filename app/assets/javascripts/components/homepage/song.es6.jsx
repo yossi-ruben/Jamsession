251e05973669889c6e11e7c4a461620411aa1d79
Tony Narisi <tonynarisi@gmail.com>
2016-09-08 14:56:05 -0400

    Get song image to appear on homepage

--- app/assets/javascripts/components/homepage/song.es6.jsx
+++ app/assets/javascripts/components/homepage/song.es6.jsx
@@ -23,7 +23,7 @@ class Song extends React.Component{
     return (
       <div id="song" className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
         <div id="song-container">
-        <img id="song-pic"src="gabegabe.jpg"/>
+        <img id="song-pic"src={this.props.theSong.img_file_path}/>
         <p id="song-title"><a id="song-title" href={`/songs/${this.props.theSong.id}`}>{this.props.theSong.title}</a></p>
         <p id="song-description">{this.props.theSong.background}</p>
         <p id="song-username"><a id="song-username" href={`/users/${this.props.theSong.owner_id}`}> {this.props.theSong.user.username}</a></p>
